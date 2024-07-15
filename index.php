9a2c5c0f0ba16a6b6edbc4e38afb48d16a53c2a6
H4NTER <mich-nikita20333@mail.ru>
2024-07-16 01:49:21 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Все сервисы НПО Андроидная Техника</title>
+</head>
+<body class="b-page">
+    <div class="b-layout">
+    <div class="page-header"> 
+            <div class="logo-header">
+                <a class="logo" href="https://npo-at.com/"> 
+                    <img src="img/logo.png" alt="logo">
+                </a>
+            </div>
+            <div class="middle-header">
+                <div class="services-tabs"> 
+                    <a href="" style="text-decoration: none;" class="used">Часто используемые</a>
+                    <a href="" style="text-decoration: none;" class="rest">Для отдыха</a>
+                    <a href="" style="text-decoration: none;" class="all">Все</a>
+                </div>
+            </div>
+            <div class="search-header">
+                <input class="search" type="text" placeholder="найти сервис">
+            </div> 
+            <div class="aut-header"> 
+                <button class="authorization" onclick="window.location.href='login.php'">Войти</button> 
+                <button class="registration" onclick="window.location.href='register.php'">Регистрация</button>
+            </div>
+    </div>
+    <hr>
+    </div>
+
+    <div class="b-line"> 
+        <h1>Сервисы и приложения</h1>
+    </div>
+
+    <div class="b-line-b-service">
+        <div class="service">
+            <h2>Часто используемые</h2>
+
+        <div class="application-card">
+
+            <div class="application-card-content">
+                <img class="l1" src="img/i1.png" alt="logo_card">
+            </div>
+            <h2 class="teg">Яндекс - поисковик</h2>
+            <p>Поиск в Яндексе, ответы Нейро, голосовой помощник Алиса, умная камера и другие возможности.</p>
+        </div>
+        </div>
+
+    </div>
+</body>
+</html
+<?php 
+echo"Привет мир!";
---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Авторизация</title>
+</head>
+<body>
+
+    <div class="logo-url">
+        <a href="https://npo-at.com/">
+            <img src="img/logo.png" alt="logo">
+        </a>
+    </div>
+    
+    <h1 class="auto">Авторизация</h1>
+
+    
+
+</body>
+</html>
